dffa04f3ce808d5c698d6a922f1d5c677a9b00bf
tomveich <veich@protonmail.com>
2023-10-10 20:28:04 +0200

    a

---
+++ assets/countdown.js
@@ -0,0 +1,29 @@
+const countDownDate = new Date("Nov 10, 2023 00:00:00").getTime();
+
+const elem = document.querySelector("#countdown");
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  elem.innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    elem.innerHTML = "NOW";
+  }
+}, 1000);
